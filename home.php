6f83be5c7590c384541c4540b9b0475c4e0e65c4
Kenold Beauplan <bkenold@gmail.com>
2017-07-27 22:58:36 -0400

    make header full width

--- home.php
+++ home.php
@@ -12,51 +12,48 @@
 </head>
 <body>
 
-    <div class="container">
-        <nav class="nav has-shadow">
-            <div class="container">
-                <div class="nav-left">
-                    <a class="nav-item" href="../index.html">
-                        movieLAKAY
-                    </a>
-                </div>
-                <span class="nav-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-                <div class="nav-right nav-menu">
-                    <a class="nav-item is-tab is-active">
-                        Home
-                    </a>
-                    <a class="nav-item is-tab">
-                        Movies
-                    </a>
-                    <a class="nav-item is-tab">
-                        People
-                    </a>
-                    <a class="nav-item is-tab">
-                        Trailers
-                    </a>
-                    <a class="nav-item is-tab">
-                        Photos
-                    </a>
-                    <a class="nav-item is-tab">
-                        News
-                    </a>
-                    <span class="nav-item">
-            <a class="button">
-              Log in
-            </a>
-            <a class="button is-info">
-              Sign up
-            </a>
-          </span>
-                </div>
+    <nav class="nav has-shadow">
+        <div class="container">
+            <div class="nav-left">
+                <a class="nav-item" href="../index.html">
+                    movieLAKAY
+                </a>
             </div>
-        </nav>
-    </div>
-
+            <span class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+            <div class="nav-right nav-menu">
+                <a class="nav-item is-tab is-active">
+                    Home
+                </a>
+                <a class="nav-item is-tab">
+                    Movies
+                </a>
+                <a class="nav-item is-tab">
+                    People
+                </a>
+                <a class="nav-item is-tab">
+                    Trailers
+                </a>
+                <a class="nav-item is-tab">
+                    Photos
+                </a>
+                <a class="nav-item is-tab">
+                    News
+                </a>
+                <span class="nav-item">
+        <a class="button">
+          Log in
+        </a>
+        <a class="button is-info">
+          Sign up
+        </a>
+      </span>
+            </div>
+        </div>
+    </nav>
 
     <!-- layout -->
     <div class="container">
