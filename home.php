3c83898ee6a25aa26dcb59eef75cd63c2358fed5
Kenold Beauplan <bkenold@gmail.com>
2017-07-27 22:38:36 -0400

    intial commit

---
+++ home.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MovieLakay - Haitian Movie Database</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" id="bulma" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.2/css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/base.css">
+</head>
+<body>
+
+        <div class="container">
+            <nav class="nav has-shadow">
+                <div class="container">
+                    <div class="nav-left">
+                        <a class="nav-item" href="../index.html">
+                            movieLAKAY
+                        </a>
+                    </div>
+                    <span class="nav-toggle">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+                    <div class="nav-right nav-menu">
+                        <a class="nav-item is-tab is-active">
+                            Home
+                        </a>
+                        <a class="nav-item is-tab">
+                            Movies
+                        </a>
+                        <a class="nav-item is-tab">
+                            People
+                        </a>
+                        <a class="nav-item is-tab">
+                            Trailers
+                        </a>
+                        <a class="nav-item is-tab">
+                            Photos
+                        </a>
+                        <a class="nav-item is-tab">
+                            News
+                        </a>
+                        <span class="nav-item">
+                <a class="button">
+                  Log in
+                </a>
+                <a class="button is-info">
+                  Sign up
+                </a>
+              </span>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+
+    <!-- layout -->
+        <section class="section newest">
+            <div class="container">
+                <h1 class="title">New Movies</h1>
+                <div class="columns">
+                    <div class="column is-three-quarters">
+                        <!-- middle poster list-->
+
+
+                        <!-- end middle poster list-->
+                    </div>
+                    <div class="column">
+                        <p class="notification is-success">sidebar</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- end layout -->
+
+        <footer class="footer">
+            <div class="container">
+                <div class="content has-text-centered">
+                    <p>
+                        &copy;<a href="http://movielakay.com">MovieLakay.com</a>
+                    </p>
+                </div>
+            </div>
+        </footer>
+
+<script async type="text/javascript" src="js/bulma.js"></script>
+</body>
+</html>
