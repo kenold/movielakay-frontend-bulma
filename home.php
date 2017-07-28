051d98c9bdb16d9025fffba2bf580172ecad21f1
Kenold Beauplan <bkenold@gmail.com>
2017-07-27 23:14:17 -0400

    link custom css file

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
     <title>MovieLakay - Haitian Movie Database</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" id="bulma" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.2/css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="../css/base.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
 </head>
 <body>
 
