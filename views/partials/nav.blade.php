3a02674562d3630c4e78a6beb91209cbcdb4b88d
Kenold Beauplan <bkenold@gmail.com>
2017-07-29 15:28:11 -0400

    update navbar for Bulma 0.4.4

--- views/partials/nav.blade.php
+++ views/partials/nav.blade.php
@@ -1,24 +1,35 @@
-<nav class="nav has-shadow">
-    <div class="container">
-        <div class="nav-left">
-            <a class="nav-item" href="home.php">movieLAKAY</a>
+<div class="container">
+    <nav class="navbar ">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="http://bulma.io">
+                <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+            </a>
+
+            <div class="navbar-burger burger" data-target="navMenuExample">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
-        <span class="nav-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </span>
-        <div class="nav-right nav-menu">
-            <a class="nav-item is-tab is-active">Home</a>
-            <a class="nav-item is-tab">Movies</a>
-            <a class="nav-item is-tab">People</a>
-            <a class="nav-item is-tab">Trailers</a>
-            <a class="nav-item is-tab">Photos</a>
-            <a class="nav-item is-tab">News</a>
-            <span class="nav-item">
-                <a class="button">Log in</a>
-                <a class="button is-info">Sign up</a>
-            </span>
+
+        <div id="navMenuExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item " href="/">Home</a>
+                <a class="navbar-item is-tab">Movies</a>
+                <a class="navbar-item is-tab">People</a>
+                <a class="navbar-item is-tab">Trailers</a>
+                <a class="navbar-item is-tab">Photos</a>
+                <a class="navbar-item is-tab">News</a>
+            </div>
+
+            <div class="navbar-end">
+                <a class="navbar-item" href="https://github.com/jgthms/bulma" target="_blank">
+                    Github
+                </a>
+                <a class="navbar-item" href="https://twitter.com/jgthms" target="_blank">
+                    Twitter
+                </a>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
