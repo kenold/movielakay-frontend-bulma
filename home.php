f464ccb167ed8e59df14a03fbff666000c66cbdb
Kenold Beauplan <bkenold@gmail.com>
2017-07-29 19:11:37 -0400

    remove columns section

--- home.php
+++ home.php
@@ -1,53 +1,38 @@
 <?php include_once 'views/partials/header.blade.php'; ?>
     <?php include_once 'views/partials/nav.blade.php'; ?>
 
-<section class="hero is-dark">
-    <div class="hero-body">
-        <!-- **** FEATURED **** -->
-        <section class="section featured">
-            <div class="container">
-                <div class="columns" id='featured'>
-                    <!-- place holder for dynamic data from custom.js -->
-                </div> <!-- /.columns #featured -->
-            </div>
-        </section> <!-- /.featured -->
-    </div>
-</section>
+    <section class="hero is-dark">
+        <div class="hero-body">
+            <!-- **** FEATURED **** -->
+            <section class="section featured">
+                <div class="container">
+                    <div class="columns" id='featured'>
+                        <!-- place holder for dynamic data from custom.js -->
+                    </div> <!-- /.columns #featured -->
+                </div>
+            </section> <!-- /.featured -->
+        </div>
+    </section>
 
     <section class="section">
         <div class="container">
             <div class="columns">
                 <div class="column is-9">
 
-                    <!-- **** NEW AND UPCOMING **** -->
-                    <section class="section new">
-                        <div class="container">
-                            <h1 class="title">New and Upcoming</h1>
-                            <div class="columns" id='new-movie'>
-                                <!-- place holder for dynamic data from custom.js -->
-                            </div> <!-- /.columns #new-movie -->
-                        </div>
-                    </section> <!-- /.new upcoming -->
+                    <h1 class="title">New and Upcoming</h1>
+                    <div class="columns" id='new-movie'>
+                        <!-- place holder for dynamic data from custom.js -->
+                    </div> <!-- /.columns #new-movie -->
 
-                    <!-- **** MOST POPULAR **** -->
-                    <section class="section popular">
-                        <div class="container">
-                            <h1 class="title">Most Popular</h1>
-                            <div class="columns" id='popular-movie'>
-                                <!-- place holder for dynamic data from custom.js -->
-                            </div> <!-- /.columns #popular-movie -->
-                        </div>
-                    </section> <!-- /.popular -->
-                </div> <!-- column is-three-quarters -->
+                    <h1 class="title">Most Popular</h1>
+                    <div class="columns" id='popular-movie'>
+                        <!-- place holder for dynamic data from custom.js -->
+                    </div> <!-- /.columns #popular-movie -->
+                </div> <!-- /.columns -->
 
-                <!-- **** SIDEBAR **** -->
-                <section class="section">
-                    <div class="container">
-                        <div class="column is-3">
-                            <p class="notification is-success">Sidebar banner advertisement here!</p>
-                        </div>
-                    </div>
-                </section>
+                <div class="column is-3 sidebar">
+                    <p class="notification is-success">Sidebar banner advertisement here!</p>
+                </div>
 
             </div> <!-- columns / main -->
         </div> <!-- /.container section -->
