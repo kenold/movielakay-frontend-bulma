859f504c21ec9b62e6d73c9e1ba7e2f7fa8c6a84
Kenold Beauplan <bkenold@gmail.com>
2017-07-29 18:50:41 -0400

    upgrade Bulma to v0.5.0

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MovieLakay - Haitian Movie Database</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" id="bulma" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.4/css/bulma.min.css" />
+    <link rel="stylesheet" id="bulma" href="css/bulma.css" />
     <link rel="stylesheet" type="text/css" href="css/custom.css">
 </head>
 <body>
