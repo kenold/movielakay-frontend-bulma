68f179194e859ce80d1c137abfdb79dbef694c47
Kenold Beauplan <bkenold@gmail.com>
2017-07-29 20:07:25 -0400

    display movie posters

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,28 @@
 //movies array
 var movies = faker.helpers.shuffle([
-    {title: "Kado Bondye", year: 2017, poster: "poster.jpg"},
-    {title: "Abused", year: 2017, poster: "poster.jpg"},
-    {title: "We Love You Anne", year: 2013, poster: "poster.jpg"},
-    {title: "The Heavenly Side of Hell", year: 2015, poster: "poster.jpg"},
-    {title: "Natalie", year: 2009, poster: "poster.jpg"},
-    {title: "Married Men", year: 2018, poster: "poster.jpg"},
-    {title: "Dancing in the Shadow of Love", year: 2013, poster: "poster.jpg"},
-    {title: "Toussaint L'ouverture", year: 2011, poster: "poster.jpg"},
-    {title: "Unconditional Love", year: 2014, poster: "poster.jpg"},
-    {title: "Ogou Le Revenant", year: 2012, poster: "poster.jpg"}
+    {title: "Kado Bondye", year: 2017, poster: "kado.jpg"},
+    {title: "Abused", year: 2017, poster: "abused.jpg"},
+    {title: "We Love You Anne", year: 2013, poster: "driver.jpg"},
+    {title: "The Heavenly Side of Hell", year: 2015, poster: "hell.jpg"},
+    {title: "Natalie", year: 2009, poster: "kidnap.jpg"},
+    {title: "Married Men", year: 2018, poster: "married.jpg"},
+    {title: "Dancing in the Shadow of Love", year: 2013, poster: "nutjob2.jpg"},
+    {title: "Toussaint L'ouverture", year: 2011, poster: "toussaint.jpg"},
+    {title: "Unconditional Love", year: 2014, poster: "love.jpg"},
+    {title: "Ogou Le Revenant", year: 2012, poster: "ogou.jpg"}
 ]);
 
 function column(i, section) {
 
     var popular = (section=='popular');
     var featured = (section=='featured');
+    var imgSrc = 'images/posters/' + movies[i].poster;
 
     return "" + "<div class='column'>" +
         "<div class='card'>" +
         "<div class='card-image'>" +
         "<figure class='image is-parent'>" +
-        "<a href='#'><img src='http://via.placeholder.com/210x311' alt='Image'></a>" +
+        "<a href='#'><img src=" + imgSrc + " alt='Image'></a>" +
         "</figure>" +
         "</div>" +
         "<div class='card-content'>" +
